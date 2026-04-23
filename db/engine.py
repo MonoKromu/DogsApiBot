46460908f745808787a5b8315d16035f104a3559
MonoKromu <naydanovss@mail.ru>
2026-04-23 21:18:04 +0700

    add sqlalchemy debug output option

--- db/engine.py
+++ db/engine.py
@@ -1,6 +1,8 @@
+import sys
+
 from sqlalchemy import create_engine
 
 from db.objects import Base
 
-engine = create_engine("sqlite:///database.db")
+engine = create_engine("sqlite:///database.db", echo=("debug" if "--debug" in sys.argv else False))
 Base.metadata.create_all(engine)
